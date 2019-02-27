bf7925e212f59a49e2bdc879e24bcdf9ed11b2fe
MrWatcher <mr.vvatcher@gmail.com>
2019-02-27 07:30:13 +0300

    upd fixedFeeParams. Delete extends from Fees(usability)

--- src/main/java/com/binance/dex/api/client/domain/FixedFeeParams.java
+++ src/main/java/com/binance/dex/api/client/domain/FixedFeeParams.java
@@ -2,9 +2,39 @@ package com.binance.dex.api.client.domain;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class FixedFeeParams extends Fees {
+class FixedFeeParams {
 
+    @JsonProperty("msg_type")
+    private String msgType;
+    @JsonProperty("fee")
+    private long fee;
+    @JsonProperty("fee_for")
+    private int feeFor;
 
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+
+    public long getFee() {
+        return fee;
+    }
+
+    public void setFee(long fee) {
+        this.fee = fee;
+    }
+
+    public int getFeeFor() {
+        return feeFor;
+    }
+
+    public void setFeeFor(int feeFor) {
+        this.feeFor = feeFor;
+    }
 }
