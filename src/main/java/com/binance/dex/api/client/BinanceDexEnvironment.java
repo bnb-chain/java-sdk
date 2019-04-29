2c2fa1388ee4a5ad97f5b8e00945147acf5eca70
qinshaohui <shawn.qin@binance.com>
2019-04-29 15:27:11 +0800

    change BinaceDexEnviornment to class instead of enum

--- src/main/java/com/binance/dex/api/client/BinanceDexEnvironment.java
+++ src/main/java/com/binance/dex/api/client/BinanceDexEnvironment.java
@@ -1,21 +1,25 @@
 package com.binance.dex.api.client;
 
-public enum BinanceDexEnvironment {
-    PROD(
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class BinanceDexEnvironment {
+    public static final BinanceDexEnvironment PROD = new BinanceDexEnvironment(
             "https://dex.binance.org",
             "wss://dex.binance.org/api/",
             "bnb"
-    ),
-    TEST_NET(
+    );
+    public static final BinanceDexEnvironment TEST_NET = new BinanceDexEnvironment(
             "https://testnet-dex.binance.org",
             "wss://testnet-dex.binance.org/api/",
             "tbnb"
-    ),
-    TEST_NET_NODE (
+    );
+    public static final BinanceDexEnvironment TEST_NET_NODE = new BinanceDexEnvironment(
             "http://data-seed-pre-0-s3.binance.org",
             "",
             "tbnb"
     );
+
     // Rest API base URL
     private String baseUrl;
     // Websocket API base URL
@@ -39,5 +43,29 @@ public enum BinanceDexEnvironment {
 
     public String getHrp() {
         return hrp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BinanceDexEnvironment that = (BinanceDexEnvironment) o;
+
+        return new EqualsBuilder()
+                .append(baseUrl, that.baseUrl)
+                .append(wsBaseUrl, that.wsBaseUrl)
+                .append(hrp, that.hrp)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(baseUrl)
+                .append(wsBaseUrl)
+                .append(hrp)
+                .toHashCode();
     }
 }
