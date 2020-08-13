e3376704ce1fb43f9b60402b243a812efdbd9f9d
fletcher.fan <fletcher.fan@binance.com>
2020-08-13 13:47:49 +0800

    remove used function

--- src/test/java/com/binance/dex/api/client/examples/BridgeNodeExample.java
+++ src/test/java/com/binance/dex/api/client/examples/BridgeNodeExample.java
@@ -82,37 +82,6 @@ public class BridgeNodeExample {
     }
 
     @Test
-    public void transferIn() throws IOException, NoSuchAlgorithmException {
-        TransferIn transferIn = new TransferIn();
-        transferIn.setContractAddress("0xd1B22dCC24C55f4d728E7aaA5c9b5a22e1512C08");
-
-        List<String> refundAddresses = new ArrayList<>();
-        refundAddresses.add("0x9fB29AAc15b9A4B7F17c3385939b007540f4d791");
-        transferIn.setRefundAddresses(refundAddresses);
-
-        List<String> receiverAddresses = new ArrayList<>();
-        receiverAddresses.add(wallet.getAddress());
-        transferIn.setReceiverAddresses(receiverAddresses);
-
-        List<Long> amounts = new ArrayList<>();
-        amounts.add(100000L);
-        transferIn.setAmounts(amounts);
-
-        transferIn.setSymbol("BNB");
-
-        transferIn.setRelayFee(new Token("BNB", 10L));
-
-        transferIn.setExpireTime((System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 3) / 1000);
-
-        TransactionOption options = new TransactionOption("", 0, null);
-
-        long sequence = nodeClient.getCurrentSequence(ClaimTypes.ClaimTypeTransferIn);
-
-        List<TransactionMetadata> results = nodeClient.transferIn(sequence, transferIn, wallet, options, true);
-        Assert.assertEquals(1, results.size());
-    }
-
-    @Test
     public void transferOut() throws IOException, NoSuchAlgorithmException {
         String toAddress = "0x9fB29AAc15b9A4B7F17c3385939b007540f4d791";
         Token token = new Token("BNB", 100000L);
@@ -146,32 +115,6 @@ public class BridgeNodeExample {
         System.out.println(EncodeUtils.toJsonStringSortKeys(results));
         Assert.assertEquals(1, results.size());
         Assert.assertTrue(results.get(0).isOk());
-    }
-
-    @Test
-    public void updateTransferOut() throws IOException, NoSuchAlgorithmException {
-        long sequence = nodeClient.getCurrentSequence(ClaimTypes.ClaimTypeUpdateTransferOut);
-        String refundAddress = wallet.getAddress();
-        Token amount = new Token("BNB", 100000L);
-        int refundReason = RefundReason.timeout;
-
-        TransactionOption options = new TransactionOption("", 0, null);
-
-        List<TransactionMetadata> results = nodeClient.updateTransferOut(sequence, refundAddress, amount, refundReason, wallet, options, true);
-        Assert.assertEquals(1, results.size());
-    }
-
-    @Test
-    public void updateBind() throws IOException, NoSuchAlgorithmException {
-        long sequence = nodeClient.getCurrentSequence(ClaimTypes.ClaimTypeUpdateBind);
-        String symbol = "BNB";
-        String contractAddress = "0xd1B22dCC24C55f4d728E7aaA5c9b5a22e1512C08";
-        int status = BindStatus.bindStatusSuccess;
-
-        TransactionOption options = new TransactionOption("", 0, null);
-
-        List<TransactionMetadata> results = nodeClient.updateBind(sequence, symbol, contractAddress, status, wallet, options, true);
-        Assert.assertEquals(1, results.size());
     }
 
     @Test
