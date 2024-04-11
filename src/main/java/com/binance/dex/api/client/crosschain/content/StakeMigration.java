823c5235dd34c7879e5c9f11d5b6dd42f6297b4a
forcodedancing <just.haha.it@gmail.com>
2024-04-11 09:06:48 +0800

    refine codes

--- src/main/java/com/binance/dex/api/client/crosschain/content/StakeMigration.java
+++ src/main/java/com/binance/dex/api/client/crosschain/content/StakeMigration.java
@@ -1,23 +1,15 @@
 package com.binance.dex.api.client.crosschain.content;
 
 import com.binance.dex.api.client.crosschain.*;
-import com.binance.dex.api.client.crosschain.content.crossStakeSynParamsBytes.DelegateSynParamsBytes;
-import com.binance.dex.api.client.crosschain.content.crossStakeSynParamsBytes.RedelegateSynParamsBytes;
-import com.binance.dex.api.client.crosschain.content.crossStakeSynParamsBytes.UndelegateSynParamsBytes;
-import com.binance.dex.api.client.encoding.ByteUtil;
 import com.binance.dex.api.client.encoding.message.common.Bech32AddressValue;
 import com.binance.dex.api.client.encoding.message.common.EthAddressValue;
-import com.binance.dex.api.client.rlp.Decoder;
-import com.binance.dex.api.client.rlp.RlpDecodable;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 
 @Getter
 @Setter
-public class StakeMigrationSyn extends Content {
+public class StakeMigration extends Content {
 
     private EthAddressValue operatorAddress;
     private EthAddressValue delegatorAddress;
